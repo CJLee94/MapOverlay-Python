676a958fc100189e0a860fcedeeef5de19f9b4be
CJLee94 <sl5954@nyu.edu>
2021-04-29 20:03:17 -0700

    TODO: 1. find a way to ensure the horizontal segments lie in the last of UC
    2. find a way to insert horizontal segment in self.t.s

--- algorithm/find_intersection.py
+++ algorithm/find_intersection.py
@@ -1,3 +1,5 @@
+import bisect
+
 from data_struct.line_segment import LineSegments
 from data_struct.event_queue import EventQueue, EPEventQueue
 from data_struct.bst import SortedSet
@@ -16,9 +18,10 @@ class SweepLine:
         plt.ion()
         self.fig, self.ax = plt.subplots()
         self.seg_list = []
-        self.U = set()
-        self.L = set()
-        self.C = set()
+        self.UC = []
+        self.LC = []
+        self.h = []
+        # self.C = []
         for seg in segments:
             seg = LineSegments(seg)
             self.seg_list.append(seg)
@@ -44,21 +47,30 @@ class SweepLine:
             p = self.q.extractMin()
             self.handle_eventpoint(p)
 
-    def add_to_set(self, seg, S):
-        S.add(seg)
-        if S is self.L:
-            seg.plot[0].set_color("green")
-        elif S is self.U:
-            seg.plot[0].set_color("red")
-        elif S is self.C:
-            seg.plot[0].set_color("yellow")
+    def add_to_sortedlist(self, seg, l):
+        if seg.horizontal:
+            l.append(seg)
+        else:
+            bisect.insort_left(l, seg)
+
+    def add_to_UC(self, seg):
+        if seg.horizontal
+        # S.add(seg)
+        # if S is self.L:
+        #     seg.plot[0].set_color("green")
+        # elif S is self.U:
+        #     seg.plot[0].set_color("red")
+        # elif S is self.C:
+        #     seg.plot[0].set_color("yellow")
 
     def clean_sets(self):
-        for S in (self.L, self.C, self.U):
-            while len(S) > 0:
-                e = S.pop()
-                if e not in self.t.s:
-                    e.plot[0].set_color("blue")
+        self.LC = []
+        self.UC = []
+
+    def check(self, l):
+        if any(element.x is None for element in l):
+            import pdb
+            pdb.set_trace()
 
     def handle_eventpoint(self, p):
         # begin the starting phase of handling the point
@@ -67,73 +79,99 @@ class SweepLine:
         plt.waitforbuttonpress()
 
         # Let U be the set of segments whose upper endpoint is p
-        self.ax.set_title("all the segments whose upper endpoint is p")
-        for e in p.incident_edge["upper"]:
-            e.sweep(p.y)
-            self.add_to_set(e, self.U)
+        # self.ax.set_title("all the segments whose upper endpoint is p")
+        for seg in p.incident_edge["upper"]:
+            seg.sweep(p.y-1e-6)
+            self.add_to_sortedlist(seg, self.UC)
+            self.check(self.UC)
+            # if seg.x == 1e12:
+            #     self.UC.append(seg)
+            # else:
+            #     bisect.insort_left(self.UC, seg)
+            # self.add_to_set(e, self.U)
 
         # self.set_color(U, "red")
-        plt.waitforbuttonpress()
+        # plt.waitforbuttonpress()
         # self.set_color(U, "blue")
 
-        for seg in self.t.s:
+        t_s = self.t.s.copy()
+        for seg in t_s:
             if seg in p.incident_edge["lower"]:
                 self.t.delete(seg)
-                self.add_to_set(seg, self.L)
+                # self.add_to_set(seg, self.L)
+                self.LC.append(seg)
+                seg.plot[0].set_color("blue")
             elif p.interior_line(seg):
                 self.t.delete(seg)
-                self.add_to_set(seg, self.C)
+                seg.sweep(p.y-1e-6)
+                self.add_to_sortedlist(seg, self.UC)
+                self.check(self.UC)
+                # bisect.insort_left(self.UC, seg)
+                self.LC.append(seg)
+                seg.plot[0].set_color("blue")
+                # self.t.insert(seg)
+                # self.add_to_set(seg, self.C)
             else:
-                self.delete(seg)
+                self.t.delete(seg)
+                seg.sweep(p.y)
+                self.add_to_sortedlist(seg, self.t.s)
+                seg.plot[0].set_color("black")
 
-        self.ax.set_title("all the segments whose lower endpoint is p")
-        # self.set_color(L, "red")
-        plt.waitforbuttonpress()
-        # self.set_color(L, "blue")
+        # self.ax.set_title("all the segments whose lower endpoint is p")
+        # # self.set_color(L, "red")
+        # plt.waitforbuttonpress()
+        # # self.set_color(L, "blue")
+        #
+        # self.ax.set_title("all the segments who contains p")
+        # # self.set_color(C, "red")
+        # plt.waitforbuttonpress()
+        # # self.set_color(C, "blue")
 
-        self.ax.set_title("all the segments who contains p")
-        # self.set_color(C, "red")
-        plt.waitforbuttonpress()
-        # self.set_color(C, "blue")
-
-        LUC = self.L | self.U | self.C
+        LUC = set(self.LC) | set(self.UC)
         if len(LUC) > 1:
             p.intersection = LUC
             self.intersection.add(p)
 
         # delete the segments in L|C from t
-        LC = self.L | self.C
-        UC = self.U | self.C
-        for e in LC:
-            self.t.delete(e)
+        # LC = self.L | self.C
+        # UC = self.U | self.C
+        # for e in self.LC:
+        #     self.t.delete(e)
             # e.plot[0].set_color("blue")
-
-        for e in UC:
-            e.sweep(p.y)
-            self.t.insert(e)
-            # e.plot[0].set_color("black")
-
-        plt.waitforbuttonpress()
 
         seg_p = LineSegments([p.x, p.y - 1, p.x, p.y + 1])
         seg_p.x = p.x
-        if len(UC) == 0:
+        for seg in self.UC:
+            # e.sweep(p.y)
+            insert_idx = bisect.bisect_right(self.t.s, seg_p)
+            self.check(self.t.s)
+            self.t.s.insert(insert_idx, seg)
+            seg.plot[0].set_color("black")
+            # e.plot[0].set_color("black")
+
+        print("Active Segments:")
+        for seg in self.t.s:
+            print("({}, {}), ({}, {})".format(seg.high.x, seg.high.y, seg.low.x, seg.low.y))
+        plt.waitforbuttonpress()
+        if len(self.UC) == 0:
             s_l = self.t.left_neighbor(seg_p)
             s_r = self.t.right_neighbor(seg_p)
             if s_l is not None and s_r is not None:
                 self.find_new_event(s_l, s_r, p)
 
         else:
-            s_prime = min(UC)
-            try:
-                s_l = self.t.left_neighbor(s_prime)
-            except:
-                import pdb
-                pdb.set_trace()
+            s_prime = self.UC[0]
+            if self.t.s.index(s_prime) - 1 >= 0:
+                s_l = self.t.s[self.t.s.index(s_prime) - 1]
+            else:
+                s_l = None
             if s_l:
                 self.find_new_event(s_l, s_prime, p)
-            s_pprime = max(UC)
-            s_r = self.t.right_neighbor(s_pprime)
+            s_pprime = self.UC[-1]
+            if self.t.s.index(s_pprime) + 1 < len(self.t.s):
+                s_r = self.t.s[self.t.s.index(s_pprime)+1]
+            else:
+                s_r = None
             if s_r:
                 self.find_new_event(s_pprime, s_r, p)
 
