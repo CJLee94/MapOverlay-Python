250c14a5d5a3c255131e3162bcd3ab766abbf00c
CJLee94 <sl5954@nyu.edu>
2021-05-08 22:27:05 -0700

    nothing just some drafts

--- algorithm/overlay.py
+++ algorithm/overlay.py
@@ -1,3 +1,4 @@
+from find_intersection import SweepLine
 def mapoverlay(s_1, s_2):
     """
 
@@ -8,5 +9,11 @@ def mapoverlay(s_1, s_2):
     """
 
     # TODO:Copy the doubly-connected edge list for s_1 and s_2 to a new doubly-connected edge list D
+    D = []
+    D.append(s_1.edge_list)
+    D.append(s_2.edge_list)
 
     # TODO: Compute all intersections between edges from s_1 and s_2 with the plane sweep algorithm of section 2.1
+    sweep_line = SweepLine(D, draw=False, verbose=False)
+    sweep
+
